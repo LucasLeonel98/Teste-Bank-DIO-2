9e6de6a306d28e40da35646c0d5d9b1d1224c207
Lucas Leonel <lucasap_98@hotmail.com>
2023-11-29 21:50:56 -0300

    removing imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,3 @@
-import {
-  Center,
-  ChakraProvider,
-  Input,
-  Box,
-  Button
-} from '@chakra-ui/react'
-import { login } from './services/login';
-import { Header } from './components/Header/Header';
-import { ButtonComp } from './components/ButtonComp';
 import { Card } from './components/Card';
 
 function App() {
